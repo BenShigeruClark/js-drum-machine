50a839243cc4593de2435a4e846857eb78175551
Ben Clark <benclarkphoto@mac.com>
2022-04-04 22:38:47 -0700

    make sequencer pads active

---
+++ app.js
@@ -0,0 +1,40 @@
+class Drumkit {
+    constructor(){
+        this.pads = document.querySelectorAll('.pad');
+        this.playBtn = document.querySelector('.play');
+        this.kickAudio = document.querySelector('.kick-sound');
+        this.hihatAudio = document.querySelector('.hihat-sound');
+        this.snareAudio = document.querySelector('.snare-sound');
+        this.index = 0;
+        this.bpm = 150;
+    }
+
+    activePad() {
+        this.classList.toggle('active');
+    }
+
+    repeat() {
+        let step = this.index % 8;
+        const activeBars = document.querySelectorAll(`.b${step}`);
+        console.log(step);
+        this.index++;
+    }
+    start() {
+        console.log(this);
+        const interval = (60/this.bpm) * 1000;
+        setInterval(() => {
+            this.repeat();
+        }, interval)
+    }
+}
+
+const drumKit = new Drumkit();
+
+drumKit.pads.forEach(pad => {
+    pad.addEventListener('click', drumKit.activePad);
+});
+
+drumKit.playBtn.addEventListener('click', function () {
+    drumKit.start();
+});
+
