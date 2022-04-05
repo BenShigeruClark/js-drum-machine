dcea4ab537189843e4303b4948dd5c4786dc9a0b
Ben Clark <benclarkphoto@mac.com>
2022-04-04 23:22:36 -0700

    add audio tracks to active pads

--- app.js
+++ app.js
@@ -16,7 +16,26 @@ class Drumkit {
     repeat() {
         let step = this.index % 8;
         const activeBars = document.querySelectorAll(`.b${step}`);
-        console.log(step);
+        // Loop over the pads
+        activeBars.forEach(bar => {
+            bar.style.animation = `playTrack 0.3s alternate ease-in-out 2`;
+            // Check if pads are active
+            if(bar.classList.contains('active')) {
+                // Check each sound
+                if(bar.classList.contains('kick-pad')) {
+                    this.kickAudio.currentTime = 0;
+                    this.kickAudio.play();
+                }
+                if(bar.classList.contains('hihat-pad')) {
+                    this.hihatAudio.currentTime = 0;
+                    this.hihatAudio.play();
+                }
+                if(bar.classList.contains('snare-pad')) {
+                    this.snareAudio.currentTime = 0;
+                    this.snareAudio.play();
+                }
+            }
+        });
         this.index++;
     }
     start() {
@@ -32,6 +51,9 @@ const drumKit = new Drumkit();
 
 drumKit.pads.forEach(pad => {
     pad.addEventListener('click', drumKit.activePad);
+    pad.addEventListener('animationend', function() {
+        this.style.animation = "";
+    });
 });
 
 drumKit.playBtn.addEventListener('click', function () {
